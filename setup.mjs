0ec674656342b30f034ab7647f39c73866a1fcfd
Jonathan Rock <Corgan.wh@gmail.com>
2024-07-10 15:54:18 -0700

    Gamemode Unlock Logic

--- setup.mjs
+++ setup.mjs
@@ -1,4 +1,4 @@
-export async function setup({ characterStorage, gameData, patch, loadTemplates, loadModule, onInterfaceAvailable, onCharacterLoaded }) {
+export async function setup({ characterStorage, gameData, patch, loadTemplates, loadModule, onModsLoaded, onInterfaceAvailable, onCharacterLoaded }) {
     console.log("Loading Divination Templates");
     await loadTemplates("templates.html"); // Add templates
   
@@ -10,7 +10,56 @@ export async function setup({ characterStorage, gameData, patch, loadTemplates, 
     console.log("Registering Divination Data");
     await gameData.addPackage('data/data.json'); // Add skill data (page + sidebar, skillData)
 
+    if(cloudManager.hasAoDEntitlementAndIsEnabled)
+        await gameData.addPackage('data/data-aod.json');
+
     console.log('Registered Divination Data.');
+
+    onModsLoaded(async () => {
+        if(cloudManager.hasAoDEntitlementAndIsEnabled) {
+            const levelCapIncreases = ['divination:Pre99Dungeons', 'divination:ImpendingDarknessSet100'];
+
+            if(cloudManager.hasTotHEntitlementAndIsEnabled) {
+                levelCapIncreases.push(...['divination:Post99Dungeons', 'divination:ThroneOfTheHeraldSet120']);
+            }
+
+            const gamemodes = game.gamemodes.filter(gamemode => gamemode.defaultInitialLevelCap !== undefined && gamemode.levelCapIncreases.length > 0 && gamemode.useDefaultSkillUnlockRequirements === true && gamemode.allowSkillUnlock === false);
+
+            await gameData.addPackage({
+                $schema: '',
+                namespace: 'divination',
+                modifications: {
+                    gamemodes: gamemodes.map(gamemode => ({
+                        id: gamemode.id,
+                        levelCapIncreases: {
+                            add: levelCapIncreases
+                        },
+                        startingSkills: {
+                            add: ['divination:Divination']
+                        },
+                        skillUnlockRequirements: [
+                            {
+                                skillID: 'divination:Divination',
+                                requirements: [
+                                    {
+                                        type: 'SkillLevel',
+                                        skillID: 'melvorD:Attack',
+                                        level: 1
+                                    }
+                                ]
+                            }
+                        ]
+                    }))
+                }
+            });
+        }
+    
+        patch(EventManager, 'loadEvents').after(() => {
+            if(game.currentGamemode.startingSkills !== undefined && game.currentGamemode.startingSkills.has(game.divination)) {
+                game.divination.setUnlock(true);
+            }
+        });
+    });
 
     onInterfaceAvailable(async () => {
         game.divination.onInterfaceAvailable();
